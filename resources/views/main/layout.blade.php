1b187ee36ee85a96c9d8a7b25f71ea69a62a508e
Suprianto D <erthru@MacBook-Pro.local>
2020-07-22 14:52:28 +0800

    setup header

--- resources/views/main/layout.blade.php
+++ resources/views/main/layout.blade.php
@@ -10,6 +10,12 @@
                 background-image: url("{{ url('/img/bg_header.jpg') }}");
             }
 
+            .header-title {
+                font-weight: bold;
+                font-size: 20px;
+                padding-top: 5px;
+            }
+
             .header-item {
                 padding: 5px 10px 5px 10px;
             }
@@ -30,6 +36,33 @@
             .current a{
                 color: #525252 !important;
             }
+
+            .btn-search:hover {
+                color: #000;
+                cursor: hand;
+            }
+
+            @media (max-width: 991px){
+                header {
+                    height: 350px;
+                }
+
+                .header-title {
+                    text-align: center;
+                }
+            }
+
+            @media (max-width: 370px){
+                .header-title {
+                    font-size: 16px;
+                }
+            }
+
+            @media (max-width: 310px){
+                .header-title {
+                    font-size: 12px;
+                }
+            }
         </style>
     </head>
 
@@ -37,6 +70,8 @@
         <header>
             <nav class="navbar navbar-expand-lg navbar-dark">                        
                 <div class="navbar-collapse">
+                    <p class="header-title text-white mt-3">PERPUSTAKAAN FAKULTAS TEKNIK</p>
+                    
                     <ul class="navbar-nav ml-auto mt-3">
                         <li class="header-item current">
                             <a href="#">Beranda</a>
@@ -57,7 +92,17 @@
                 </div>
             </nav>
 
-
+            <div class="row justify-content-center mt-4">
+                <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                    <form class="pr-4 pl-4" method="get" action="/search">
+                        <input class="form-control text-center" type="search" placeholder="Cari buku/skripsi..." name="q" style="border-radius: 20px" required>
+                        
+                        <div class="d-flex justify-content-center mt-2">
+                            <button class="btn btn-outline-light w-50 btn-search" type="submit" style="border-radius: 20px">Cari</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </header>
 
         <main class="container">

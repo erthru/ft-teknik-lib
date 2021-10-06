7b9efa4b5a7eafe48d3553fc8d8295a7746d8357
Suprianto Djmlu <erthru@Supriantos-MacBook-Pro.local>
2021-10-06 20:44:15 +0800

    update composer, window.print

--- resources/views/admin/member_cbp.blade.php
+++ resources/views/admin/member_cbp.blade.php
@@ -1,173 +1,175 @@
 @extends("admin.layout")
 @section("title", "Admin Dashboard Kartu Anggota")
 @section("content")
-    <style>
-        .printAreaCBP {
+<style>
+    .printAreaCBP {
+        visibility: hidden;
+        width: 65%;
+    }
+
+    @media print {
+
+        html,
+        body {
+            background-color: #FFF;
             visibility: hidden;
-            width: 65%;
+            margin-top: -170px;
         }
 
-        @media print {
-            html, body {
-                background-color: #FFF;
-                visibility: hidden;
-                margin-top: -170px;
-            }
-
-            .printAreaCBP {
-                visibility: visible;
-                width: 100% !important;
-            }
+        .printAreaCBP {
+            visibility: visible;
+            width: 100% !important;
         }
-    </style>
+    }
+</style>
 
-    <div class="mt-4">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="/">Beranda</a></li>
-                <li class="breadcrumb-item"><a href="/admin">Admin</a></li>
-                <li class="breadcrumb-item"><a href="/admin/member">Anggota</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Cek Bebas Perpustakaan</li>
-            </ol>
-        </nav>
+<div class="mt-4">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/">Beranda</a></li>
+            <li class="breadcrumb-item"><a href="/admin">Admin</a></li>
+            <li class="breadcrumb-item"><a href="/admin/member">Anggota</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Cek Bebas Perpustakaan</li>
+        </ol>
+    </nav>
 
-        <div class="card">
-            <div class="card-header bg-light">
-                <strong>Cek Bebas Perpustakaan</strong>
+    <div class="card">
+        <div class="card-header bg-light">
+            <strong>Cek Bebas Perpustakaan</strong>
+        </div>
+
+        <div class="card-body">
+            @if(count($loanIsNotPaid) > 0)
+            <div class="alert alert-danger">
+                Masih terdapat beberapa peminjaman yang belum dikembalikan atau mempunyai denda/hilang dan belum diganti/dibayarkan
+                <br />
+                <a href="/admin/loan">Cek Data Peminjaman</a>
+            </div>
+            @else
+            <div class="alert alert-success">
+                Semua peminjaman tidak ada masalah
             </div>
 
-            <div class="card-body">
-                @if(count($loanIsNotPaid) > 0)
-                    <div class="alert alert-danger">
-                        Masih terdapat beberapa peminjaman yang belum dikembalikan atau mempunyai denda/hilang dan belum diganti/dibayarkan
-                        <br />
-                        <a href="/admin/loan">Cek Data Peminjaman</a>
-                    </div>
-                @else
-                    <div class="alert alert-success">
-                        Semua peminjaman tidak ada masalah
-                    </div>
+            <button class="btn btn-success mt-2" onclick="printExecute()">Cetak Bebas Perpustakaan</button>
+            @endif
+        </div>
+    </div>
 
-                    <button class="btn btn-success mt-2" onclick="printExecute()">Cetak Bebas Perpustakaan</button>
-                @endif
+    @if(count($loanIsNotPaid) == 0)
+    <div class="printAreaCBP mx-auto text-justify" style="width: 65%;">
+        <div class="d-flex" style="font-family: 'Times New Roman'">
+            <img class="mr-2" src="{{ url('/img/ung.png') }}" alt="" height="120" width="120" />
+
+            <div class="text-center">
+                <div class="h5">
+                    <strong>
+                        KEMENTRIAN PENDIDIKAN DAN KEBUDAYAAN
+                        <br />
+                        UNIVERSITAS NEGERI GORONTALO
+                        <br />
+                        FAKULTAS TEKNIK
+                    </strong>
+                </div>
+
+                <div>
+                    Jl. B.J Habibie Desa Moutong Kecamatan Tilong Kabila Kab. Bone Bolango
+                    <br />
+                    Telepon: (0435) 821183 Fax (0435) 821752
+                    <br />
+                    Laman: http://www.ft.ung.ac.id
+                </div>
             </div>
         </div>
 
-        @if(count($loanIsNotPaid) == 0) 
-            <div class="printAreaCBP mx-auto text-justify" style="width: 65%;">
-                <div class="d-flex" style="font-family: 'Times New Roman'">
-                    <img class="mr-2" src="{{ url('/img/ung.png') }}" alt="" height="120" width="120" />
 
-                    <div class="text-center">
-                        <div class="h5">
-                            <strong>
-                                KEMENTRIAN PENDIDIKAN DAN KEBUDAYAAN
-                                <br />
-                                UNIVERSITAS NEGERI GORONTALO
-                                <br />
-                                FAKULTAS TEKNIK
-                            </strong>
-                        </div>
+        <div style="width: 100%; height: 4px; background-color: #000"></div>
 
-                        <div>
-                            Jl. B.J Habibie Desa Moutong Kecamatan Tilong Kabila Kab. Bone Bolango
-                            <br />
-                            Telepon: (0435) 821183 Fax (0435) 821752
-                            <br />
-                            Laman: http://www.ft.ung.ac.id
-                        </div>
-                    </div>
-                </div>
-                
+        <div class="mt-4 text-center w-100">
+            <p class="h5"><ins>SURAT KETERANGAN BEBAS PERPUSTAKAAN</ins></p>
+            <p style="margin-top: -10px">Nomor: B/89/UN47 TA.01.02/PERPUS-FT/LL/2020</p>
+        </div>
 
-                <div style="width: 100%; height: 4px; background-color: #000"></div>
+        <p>Kepala Perpustakaan Fakultas Teknik Universitas Negeri Gorontalo Menerangkan Bahwa:</p>
 
-                <div class="mt-4 text-center w-100">
-                    <p class="h5"><ins>SURAT KETERANGAN BEBAS PERPUSTAKAAN</ins></p>
-                    <p style="margin-top: -10px">Nomor: B/89/UN47 TA.01.02/PERPUS-FT/LL/2020</p>
-                </div>
+        <table>
+            <thead>
+                <tr>
+                    <td style="width: 20%"></td>
+                    <td></td>
+                </tr>
+            </thead>
 
-                <p>Kepala Perpustakaan Fakultas Teknik Universitas Negeri Gorontalo Menerangkan Bahwa:</p>
+            <tbody>
+                <tr>
+                    <td>Nama</td>
+                    <td>: {{ $member->full_name }}</td>
+                </tr>
 
-                <table>
-                    <thead>
-                        <tr>
-                            <td style="width: 20%"></td>
-                            <td></td>
-                        </tr>
-                    </thead>
+                <tr>
+                    <td>NIM</td>
+                    <td>: {{ $member->nim }}</td>
+                </tr>
 
-                    <tbody>
-                        <tr>
-                            <td>Nama</td>
-                            <td>: {{ $member->full_name }}</td>
-                        </tr>
+                <tr>
+                    <td>No. KTA</td>
+                    <td>: -</td>
+                </tr>
 
-                        <tr>
-                            <td>NIM</td>
-                            <td>: {{ $member->nim }}</td>
-                        </tr>
+                <tr>
+                    <td>Jurusan</td>
+                    <td>: {{ $member->major->name }}</td>
+                </tr>
 
-                        <tr>
-                            <td>No. KTA</td>
-                            <td>: -</td>
-                        </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td>: {{ $member->address }}</td>
+                </tr>
+            </tbody>
+        </table>
 
-                        <tr>
-                            <td>Jurusan</td>
-                            <td>: {{ $member->major->name }}</td>
-                        </tr>
+        <p style="margin-top: 10px">Mahasisiwa yang namanya tersebut diatas dinyatakan telah melunasi kewajibannya berupa:</p>
 
-                        <tr>
-                            <td>Alamat</td>
-                            <td>: {{ $member->address }}</td>
-                        </tr>
-                    </tbody>
-                </table>
+        <div style="margin-top: -10px">
+            <span>1. Mengembalikan buku pinjaman</span>
+            <br />
+            <span>2. Memberikan partisipasi uang tunai sebagai pengganti sumbangan buku senilai Rp. 50.000,- (Lima Puluh Ribu Rupiah).</span>
+        </div>
 
-                <p style="margin-top: 10px">Mahasisiwa yang namanya tersebut diatas dinyatakan telah melunasi kewajibannya berupa:</p>
-                
-                <div style="margin-top: -10px">
-                    <span>1. Mengembalikan buku pinjaman</span>
-                    <br />
-                    <span>2. Memberikan partisipasi uang tunai sebagai pengganti sumbangan buku senilai Rp. 50.000,- (Lima Puluh Ribu Rupiah).</span>
-                </div>
+        <p style="margin-top: 10px;">Untuk itu kepada yang bersangkutan dinyatakan telah bebas dari segala kewajiban perpustakaan Fakultas Teknik Universitas Negeri Gorontalo dan berhak mendapatkan surat keterangan ini</p>
 
-                <p style="margin-top: 10px;">Untuk itu kepada yang bersangkutan dinyatakan telah bebas dari segala kewajiban perpustakaan Fakultas Teknik Universitas Negeri Gorontalo dan berhak mendapatkan surat keterangan ini</p>
+        <p style="margin-top: 10px;">Demikian surat keterangan ini dibuat, untuk dapat dipergunakan seperlunya dan atas kerja sama yang baik diucapkan terima kasih.</p>
 
-                <p style="margin-top: 10px;">Demikian surat keterangan ini dibuat, untuk dapat dipergunakan seperlunya dan atas kerja sama yang baik diucapkan terima kasih.</p>
-            
-                <div class="d-flex" style="margin-top: 40px;">
-                    <div class="ml-auto" style="margin-right: 40px">
-                        @php 
-                            function monthToId($index) {
-                                if($index == "01") return "Januari";
-                                if($index == "02") return "Februari";
-                                if($index == "03") return "Maret";
-                                if($index == "04") return "April";
-                                if($index == "05") return "Mei";
-                                if($index == "06") return "Juni";
-                                if($index == "07") return "Juli";
-                                if($index == "08") return "Agustus";
-                                if($index == "09") return "September";
-                                if($index == "10") return "Oktober";
-                                if($index == "11") return "November";
-                                return "Desember";
-                            }
-                        @endphp
+        <div class="d-flex" style="margin-top: 40px;">
+            <div class="ml-auto" style="margin-right: 40px">
+                @php
+                function monthToId($index) {
+                if($index == "01") return "Januari";
+                if($index == "02") return "Februari";
+                if($index == "03") return "Maret";
+                if($index == "04") return "April";
+                if($index == "05") return "Mei";
+                if($index == "06") return "Juni";
+                if($index == "07") return "Juli";
+                if($index == "08") return "Agustus";
+                if($index == "09") return "September";
+                if($index == "10") return "Oktober";
+                if($index == "11") return "November";
+                return "Desember";
+                }
+                @endphp
 
-                        <p>Gorontalo, {{ date("d") . " " . monthToId(date("m")) . " " . date("Y") }}</p>
-                        <p style="margin-top: -16px">Kepala Perpustakaan</p>
-                        <p style="margin-top: 100px;">Indhitya R. Padiku, S.Kom, M.Kom</p>
-                    </div>
-                </div>
+                <p>Gorontalo, {{ date("d") . " " . monthToId(date("m")) . " " . date("Y") }}</p>
+                <p style="margin-top: -16px">Kepala Perpustakaan</p>
+                <p style="margin-top: 100px;">Indhitya R. Padiku, S.Kom, M.Kom</p>
             </div>
-        @endif
+        </div>
     </div>
+    @endif
+</div>
 
-    <script>
-        function printExecute() {
-            print();
-        }
-    </script>
+<script>
+    function printExecute() {
+        window.print();
+    }
+</script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
+|w
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::prefix("")->group(function (){
+Route::prefix("")->group(function () {
     Route::get("", "ItemController@itemHome");
     Route::get("search", "ItemController@itemSearch");
-    Route::get("help", function() { return view("main.help"); });
-    Route::get("about", function() { return view("main.about"); });
+    Route::get("help", function () {
+        return view("main.help");
+    });
+    Route::get("about", function () {
+        return view("main.about");
+    });
 });
 
-Route::prefix("admin")->group(function (){
+Route::prefix("admin")->group(function () {
     Route::get("", "AdminController@adminDashboard");
     Route::get("login", "AdminController@adminLogin");
     Route::get("logout", "AdminController@adminLogoutAction");
@@ -28,31 +32,31 @@ Route::prefix("admin")->group(function (){
     Route::post("login", "AdminController@adminLoginAction");
     Route::post("change_password", "AdminController@adminChangePasswordAction");
 
-    Route::prefix("book")->group(function (){
+    Route::prefix("book")->group(function () {
         Route::get("", "ItemController@book");
         Route::get("add", "ItemController@bookAdd");
         Route::get("detail", "ItemController@bookDetail");
         Route::get("delete", "ItemController@bookDeleteAction");
         Route::get("delete_file", "ItemController@bookDeleteFileAction");
         Route::get("json/datatable/group_by_all_exclude_code", "ItemController@dataTableBookGroupByAllExculeCodeJSON");
-        route::get("json/data/search_item_available_to_borrow","ItemController@dataItemSearchItemAvailableToBorrowJSON");
+        route::get("json/data/search_item_available_to_borrow", "ItemController@dataItemSearchItemAvailableToBorrowJSON");
         Route::post("add", "ItemController@bookAddAction");
         Route::post("update", "ItemController@bookUpdateAction");
     });
 
-    Route::prefix("essay")->group(function (){
+    Route::prefix("essay")->group(function () {
         Route::get("", "ItemController@essay");
         Route::get("add", "ItemController@essayAdd");
         Route::get("detail", "ItemController@essayDetail");
         Route::get("delete", "ItemController@essayDeleteAction");
         Route::get("delete_file", "ItemController@essayDeleteFileAction");
         Route::get("json/datatable/group_by_all_exclude_code", "ItemController@dataTableEssayGroupByAllExculeCodeJSON");
-        route::get("json/data/search_item_available_to_borrow","ItemController@dataItemSearchItemAvailableToBorrowJSON");
+        route::get("json/data/search_item_available_to_borrow", "ItemController@dataItemSearchItemAvailableToBorrowJSON");
         Route::post("add", "ItemController@essayAddAction");
         Route::post("update", "ItemController@essayUpdateAction");
     });
 
-    Route::prefix("major")->group(function (){
+    Route::prefix("major")->group(function () {
         Route::get("", "MajorController@major");
         Route::get("add", "MajorController@majorAdd");
         Route::get("detail", "MajorController@majorDetail");
@@ -62,7 +66,7 @@ Route::prefix("admin")->group(function (){
         Route::post("update", "MajorController@majorUpdateAction");
     });
 
-    Route::prefix("study_program")->group(function (){
+    Route::prefix("study_program")->group(function () {
         Route::get("", "StudyProgramController@studyProgram");
         Route::get("add", "StudyProgramController@studyProgramAdd");
         Route::get("detail", "StudyProgramController@studyProgramDetail");
@@ -73,7 +77,7 @@ Route::prefix("admin")->group(function (){
         Route::post("update", "StudyProgramController@studyProgramUpdateAction");
     });
 
-    Route::prefix("member")->group(function (){
+    Route::prefix("member")->group(function () {
         Route::get("", "MemberController@member");
         Route::get("add", "MemberController@memberAdd");
         Route::get("detail", "MemberController@memberDetail");
@@ -86,7 +90,7 @@ Route::prefix("admin")->group(function (){
         Route::post("update", "MemberController@memberUpdateAction");
     });
 
-    Route::prefix("loan")->group(function (){
+    Route::prefix("loan")->group(function () {
         Route::get("", "LoanController@loan");
         Route::get("add", "LoanController@loanAdd");
         Route::get("detail", "LoanController@loanDetail");
